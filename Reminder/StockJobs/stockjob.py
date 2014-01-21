e21587ae4c8508584be6313f5bb18bab50740d5a
EC2 Default User <ec2-user@ip-172-31-36-148.us-west-2.compute.internal>
2014-01-21 02:16:25 +0000

    Add time specification

--- Reminder/StockJobs/stockjob.py
+++ Reminder/StockJobs/stockjob.py
@@ -2,12 +2,35 @@ from Reminder.WebCrawlers import webcrawler
 from Reminder.EmailManager import emailmanager
 from Reminder.ConditionManagers import conditionmanager
 
+import datetime
+import sys
+
 class StockJob:
     webcrawler = None
+    daytime_url = 'http://www.nasdaq.com/symbol/tsla'
+    after_hours_url = 'http://www.nasdaq.com/symbol/tsla/after-hours'
+    premarket_url = 'http://www.nasdaq.com/symbol/tsla/premarket'
     emailmanager = None
     conditionmanager = None
     def __init__(self):
-        self.webcrawler = webcrawler.WebCrawler('http://www.nasdaq.com/symbol/tsla', 'qwidget-dollar')
+        time_del = datetime.timedelta(hours=8)
+	now = datetime.datetime.now() - time_del
+	premarket_start = now.replace(hour=2, minute=0, second=0, microsecond=0)
+	market_open = now.replace(hour=6, minute=0, second=0, microsecond=0)
+	market_close = now.replace(hour=13, minute=0, second=0, microsecond=0)
+	after_hours_close = now.replace(hour=17, minute=0, second=0, microsecond=0)
+	print now, '     :        '	
+	if now < premarket_start or now > after_hours_close:
+	    print 'market not open. exit'
+	    sys.exit(0)
+	curl_url = self.daytime_url
+	if now < market_open:
+	    print 'using premarket'
+	    curl_url = self.premarket_url
+	elif now > market_close:
+	    print 'using after hours'
+	    curl_url = self.after_hours_url
+	self.daytimewebcrawler = webcrawler.WebCrawler(curl_url, 'qwidget-dollar')
         self.emailmanager = emailmanager.EmailManager()
         self.conditionmanager = conditionmanager.ConditionManager()
 
