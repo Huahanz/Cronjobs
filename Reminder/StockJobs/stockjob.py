6284b425b2e87c0b26f3ec037c6ec30d4e032312
EC2 Default User <ec2-user@ip-172-31-36-148.us-west-2.compute.internal>
2014-01-24 19:43:24 +0000

    Add list load

--- Reminder/StockJobs/stockjob.py
+++ Reminder/StockJobs/stockjob.py
@@ -1,18 +1,22 @@
 from Reminder.WebCrawlers import webcrawler
 from Reminder.EmailManager import emailmanager
 from Reminder.ConditionManagers import conditionmanager
+from Reminder.Models import stockmodel
 
 import datetime
 import sys
 
 class StockJob:
-    webcrawler = None
-    daytime_url = 'http://www.nasdaq.com/symbol/tsla'
-    after_hours_url = 'http://www.nasdaq.com/symbol/tsla/after-hours'
-    premarket_url = 'http://www.nasdaq.com/symbol/tsla/premarket'
+    nasdaq_url_prefix = 'http://www.nasdaq.com/symbol/'
+    nasdaq_after_hours_suffix = '/after-hours'
+    nasdaq_permarket_suffix = '/premarket'
     emailmanager = None
     conditionmanager = None
     def __init__(self):
+	self.emailmanager = emailmanager.EmailManager()
+        self.conditionmanager = conditionmanager.ConditionManager()
+
+    def set_env(self, stock_obj):
         time_del = datetime.timedelta(hours=8)
 	now = datetime.datetime.now() - time_del
 	premarket_start = now.replace(hour=2, minute=0, second=0, microsecond=0)
@@ -23,25 +27,34 @@ class StockJob:
 	if now < premarket_start or now > after_hours_close:
 	    print 'market not open. exit'
 	    sys.exit(0)
-	curl_url = self.daytime_url
+	curl_url = self.nasdaq_url_perfix + stock_obj.symbol 
 	if now < market_open:
 	    print 'using premarket'
-	    curl_url = self.premarket_url
+	    curl_url += self.nasdaq_permarket_suffix
 	elif now > market_close:
 	    print 'using after hours'
+	    curl_url += self.nasdaq_after_hours_suffix
 	else:
 	    print 'normal hours'
-        curl_url = self.after_hours_url
-	self.webcrawler = webcrawler.WebCrawler(curl_url, 'qwidget-dollar')
-        self.emailmanager = emailmanager.EmailManager()
-        self.conditionmanager = conditionmanager.ConditionManager()
+        webcrawler = webcrawler.WebCrawler(curl_url, stock_obj.pattern)	
+        return webcrawler
 
-    def run(self):
-        if self.webcrawler:
-            result = self.webcrawler.search_pattern()
+    def run(self, webcrawler, stock_obj):
+        if webcrawler:
+            result = webcrawler.search_pattern()
             if result:
-                if self.conditionmanager.is_int_larger_than(result, 180):
+                if self.conditionmanager.is_int_larger_than(result, stock_obj.max) or self.conditionmanager.is_int_lower_than(result, stock_obj.min):
                     self.emailmanager.send_email_to_single_address_gmail('huahanzh@gmail.com', 'huahanzh@gmail.com', 'testemail123', 'alert', result)
 
+    def run_list(self):
+	tsla = stockmodel.StockModel('tsla', 'qwidget-dollar', 130, 190)
+        yahoo = stockmodel.StockModel('yhoo', 'qwidget-dollar', 35, 45) 
+        bac = models.StockModel('bac', 'qwidget-dollar', 14, 18)
+	stock_list = [tsla, yahoo, bac] 
+	for stock_obj in stock_list:
+	    webcrawler = self.set_env(stock_obj)
+	    self.run(webcrawler, stock_obj)
+	    	
+
 sj = StockJob()
-sj.run()
+sj.run_list()
