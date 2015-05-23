335b1a96955ad419405b2e171580cd71cc26a5bb
huahanz <huahanz@uci.edu>
2015-05-23 22:09:07 +0000

    python push notification lib

---
+++ PyAPNs/exe.py
@@ -0,0 +1,18 @@
+import time
+from apns import APNs, Frame, Payload
+
+apns = APNs(use_sandbox=True, cert_file='LocalTestCert.pem', key_file='LocalTestKeyNP.pem')
+
+# Send a notification
+token_hex = 'd6915bfdc113c048a04666f61b733916ec23ef6db0c0b2c1081ec9f721df8c33'
+payload = Payload(alert="Hello World!", sound="default", badge=1)
+apns.gateway_server.send_notification(token_hex, payload)
+
+# Send multiple notifications in a single transmission
+frame = Frame()
+identifier = 1
+expiry = time.time()+3600
+priority = 10
+frame.add_item('d6915bfdc113c048a04666f61b733916ec23ef6db0c0b2c1081ec9f721df8c33', payload, identifier, expiry, priority)
+apns.gateway_server.send_notification_multiple(frame)
+
