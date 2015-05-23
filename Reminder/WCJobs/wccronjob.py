43b240195d88fa80c9241d703231bcd03436e91a
Huahan <huahan@Huahans-MacBook-Pro.local>
2015-05-23 15:19:04 -0700

    remote debug

--- Reminder/WCJobs/wccronjob.py
+++ Reminder/WCJobs/wccronjob.py
@@ -5,32 +5,33 @@ from threading import Thread
 from time import sleep
 import datetime
 import sys
+import time
+from apns import APNs, Frame, Payload
+
 
 class WCCronJob:
-    # nasdaq_url_prefix = 'http://www.nasdaq.com/symbol/'
-    # nasdaq_after_hours_suffix = '/after-hours'
-    # nasdaq_premarket_suffix = '/premarket'
-    # body = ""
-    # earning_report = False
-    # stock_alert = False
-    # url_suffix = ""
-    # nasdaq_pattern = "qwidget-dollar"
-    # wc = None
-    # scm = None
-    # enter_time = None
-    # TEST_MODE = False
-    # EARNING_REPORT_RANGE = 23
-    # TEXT_THRESHOLD = 1
-    #
     def __init__(self):
         self.enter_time = self.get_now()
         self.wc = webcrawler.WebCrawler()
+        self.apns = APNs(use_sandbox=True, cert_file='LocalTestCert.pem', key_file='LocalTestKeyNP.pem')
+
+        # Send a notification
+        # token_hex = 'd6915bfdc113c048a04666f61b733916ec23ef6db0c0b2c1081ec9f721df8c33'
+        # self.apns.gateway_server.send_notification(token_hex, payload)
+
+        # Send multiple notifications in a single transmission
+        self.frame = Frame()
+        self.identifier = 1
+        self.expiry = time.time() + 3600
+        self.priority = 10
+
         pass
 
     def set_env(self):
         pass
 
-    def get_now(self):
+    @staticmethod
+    def get_now():
         time_del = datetime.timedelta(hours=8)
         return datetime.datetime.now() - time_del
 
@@ -41,6 +42,9 @@ class WCCronJob:
         result = self.wc.search_pattern(url, wj.pattern)
         if result:
             msg += 'Found' + url + ':' + result
+            device_token = 'd6915bfdc113c048a04666f61b733916ec23ef6db0c0b2c1081ec9f721df8c33'
+            payload = Payload(alert="Hello World!", sound="default", badge=1)
+            self.add_to_push_notification(device_token, payload)
         else:
             msg += 'NOT Found'
         print msg
@@ -58,6 +62,12 @@ class WCCronJob:
 
         for thread in threads:
             thread.join()
+        self.apns.gateway_server.send_notification_multiple(self.frame)
+
+    def add_to_push_notification(self, device_token, payload):
+        self.frame.add_item(device_token, payload, self.identifier, self.expiry,
+                            self.priority)
+
 
 wc = WCCronJob()
 wc.run_by_list()
